e2699bcecaa38594c0f0007f17014470f7d9f5c8
huaidou <huaidou@2dfire.com>
2017-11-30 21:52:16 +0800

    毕业设计

--- src/test/java/cn/edu/zzti/bibased/utilsTests/HttpTests.java
+++ src/test/java/cn/edu/zzti/bibased/utilsTests/HttpTests.java
@@ -10,8 +10,7 @@ public class HttpTests {
 
     
     public static void main(String[] args) {
-    
-        String url = "http://search.51job.com/list/080200,000000,0000,00,9,99,Java%2B%25E5%25BC%2580%25E5%258F%2591,2,1.html?lang=c&stype=1&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=1&dibiaoid=0&address=&line=&specialarea=00&from=&welfare=";
+        String url="https://www.lagou.com/jobs/positionAjax.json?jd=C%E8%BD%AE&px=default&city=%E4%B8%8A%E6%B5%B7&needAddtionalResult=false&isSchoolJob=0";
         String s = MyHttpClientUtils.doHttpCilentGet(url);
         Document parse = Jsoup.parse(s);
         Element head = parse.head();
