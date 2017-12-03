6d5b780af83cc3571e31868dc50d8fe1630978ff
huaidou <huaidou@2dfire.com>
2017-12-03 22:31:20 +0800

    毕业设计

--- src/main/java/cn/edu/zzti/bibased/thread/LaGouTask.java
+++ src/main/java/cn/edu/zzti/bibased/thread/LaGouTask.java
@@ -1,21 +1,24 @@
 package cn.edu.zzti.bibased.thread;
 
 import cn.edu.zzti.bibased.service.HttpClientService;
+import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import java.util.concurrent.Callable;
 
 /**
  * 拉钩网的获取数据线程
  * 有返回值的
  */
-public class LaGouThread implements Callable {
-    private String apiUrl;
+@Component
+public class LaGouTask implements Callable {
+   //final private String apiUrl;
+    @Resource
     private HttpClientService HttpClientService;
-
-    public LaGouThread(String apiUrl, HttpClientService httpClientService) {
-        this.apiUrl = apiUrl;
-        HttpClientService = httpClientService;
-    }
+//
+//    public LaGouTask(String apiUrl) {
+//        this.apiUrl = apiUrl;
+//    }
 
     @Override
     public Object call() throws Exception {
