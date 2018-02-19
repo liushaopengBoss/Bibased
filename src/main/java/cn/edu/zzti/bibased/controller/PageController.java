bde2f97f4f9d08e3065e09d4bf543e36f31c4059
huaidou <huaidou@2dfire.com>
2018-02-19 18:21:37 +0800

    毕业设计

--- src/main/java/cn/edu/zzti/bibased/controller/PageController.java
+++ src/main/java/cn/edu/zzti/bibased/controller/PageController.java
@@ -15,9 +15,11 @@ public class PageController {
     private static final String INDEX= PAGE_HOME + "index";
     private static final String INDEHOMEPAGE= PAGE_HOME + "index_homepage";
     private static final String CHARTS= PAGE_HOME + "charts";
-
+    private static final String PROJECT= PAGE_HOME + "projects";
+    private static final String PROJECT_DETAIL= PAGE_HOME + "project_detail";
     /**
      * 首页查询{主页}
+     *
      * @return
      */
     @RequestMapping(value = "/index")
@@ -25,13 +27,40 @@ public class PageController {
         return INDEX;
     }
 
+    /**
+     * 首页
+     *
+     * @return
+     */
     @RequestMapping(value = "/index_homepage")
     public String homepage() {
         return INDEHOMEPAGE;
     }
-
+    /**
+     *图表分析
+     *
+     * @return
+     */
     @RequestMapping(value = "/charts")
     public String charts() {
         return CHARTS;
+    }
+    /**
+     *项目采集信息
+     *
+     * @return
+     */
+    @RequestMapping(value = "/projects")
+    public String project() {
+        return PROJECT;
+    }
+    /**
+     *项目采集详情
+     *
+     * @return
+     */
+    @RequestMapping(value = "/project_detail")
+    public String projectDetail() {
+        return PROJECT_DETAIL;
     }
 }
