64e19df2ff5a91cf15be23cec426c3e1764f2737
huaidou <huaidou@2dfire.com>
2018-04-24 17:07:50 +0800

    毕业设计

--- src/main/java/cn/edu/zzti/bibased/dto/query/PositionDetailQuery.java
+++ src/main/java/cn/edu/zzti/bibased/dto/query/PositionDetailQuery.java
@@ -7,6 +7,33 @@ import java.util.List;
 
 public class PositionDetailQuery  extends PositionDetail implements Serializable{
     private List<String> cityList;
+    private int pageRow;
+    private int pageSize;
+    private int totalPageRow;
+
+    public int getPageRow() {
+        return pageRow;
+    }
+
+    public void setPageRow(int pageRow) {
+        this.pageRow = pageRow;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public int getTotalPageRow() {
+        return totalPageRow;
+    }
+
+    public void setTotalPageRow(int totalPageRow) {
+        this.totalPageRow = totalPageRow;
+    }
 
     public List<String> getCityList() {
         return cityList;
